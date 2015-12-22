a7924a65127c841134c4d89b326aba5a6c827fad
Tristan Harris <tris.dev@pumpcottage.me.uk>
2015-12-22 22:25:11 +0000

    WIP: Send user data to Arduino

--- control.rb
+++ control.rb
@@ -1,4 +1,12 @@
 require "serialport"
+require 'active_record'
+require './request'
+
+ActiveRecord::Base.establish_connection({adapter: "sqlite3", database: "dev.sqlite3"})
+
+v1 = Request.average(:v1)
+v2 = Request.average(:v2)
+puts [v1.to_i,v2.to_i].inspect
 
 #params for serial port
 port_str = "/dev/ttyACM0"  #may be different for you
@@ -9,7 +17,7 @@ parity = SerialPort::NONE
 
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
-sp.puts "50,50\n"
+sp.puts [v1.to_i,v2.to_i].join(',') + "\n"
 #just read forever
 while true do
    while (i = sp.gets) do
